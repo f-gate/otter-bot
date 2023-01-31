5fae09842e6887f32fe8a86b228a72a3604eb4d2
gatsey <felixgate13@gmail.com>
2023-01-31 10:58:47 +0000

    proper loop, invitestring, convert to recipiant

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::hash::{Hash, Hasher};
 use teloxide::{prelude::*, update_listeners::webhooks};
 use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
 use teloxide::error_handlers::IgnoringErrorHandler;
-use teloxide::types::UserId;
+use teloxide::types::{UserId, Recipient};
 
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
@@ -43,9 +43,9 @@ pub enum State {
     Start,
     ReceiveFullName,
     WhoWith {full_name: String},
-    Location {full_name: String, referee: UserId},
-    TypeOfPersonList {full_name: String, referee: UserId, location: String},
-    WhatAreYouBuilding {full_name: String, referee: UserId, location: String, person: PersonType},
+    Location {full_name: String, referee: Recipient},
+    TypeOfPersonList {full_name: String, referee: Recipient, location: String},
+    WhatAreYouBuilding {full_name: String, referee: Recipient, location: String, person: PersonType},
 }
 
 #[tokio::main]
@@ -68,7 +68,7 @@ async fn main() {
                 .branch(dptree::case![State::Start].endpoint(start))
                 .branch(dptree::case![State::ReceiveFullName].endpoint(ask_full_name))
                 .branch(dptree::case![State::WhoWith { full_name }].endpoint(ask_who_with))
-                //.branch(dptree::case![State::Location { full_name, referee }].endpoint(ask_location))
+                .branch(dptree::case![State::Location { full_name, referee }].endpoint(ask_location))
                 //.branch(dptree::case![State::TypeOfPersonList { full_name, referee, location }].endpoint(ask_type))
                 //.branch(dptree::case![State::WhatAreYouBuilding { full_name, referee, location, person } ].endpoint(ask_what))
         )
@@ -90,7 +90,7 @@ async fn ask_full_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerR
     match msg.text() {
         Some(text) => {
             bot.send_message(msg.chat.id, format!("Hello {}", text)).await?;
-            bot.send_message(msg.chat.id, format!("And with whom are you chatting with? (@alexanderfive, @xxx, @yyy, or @zzz) ")).await?;
+            bot.send_message(msg.chat.id, format!("And with whom are you chatting with? (@alexanderfive, (the only one that works atm)) ")).await?;
 
             dialogue.update(State::WhoWith { full_name: String::from(text) }).await?;
 
@@ -102,20 +102,42 @@ async fn ask_full_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerR
     Ok(())
 }
 
-async fn ask_who_with(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+async fn ask_who_with(bot: Bot, dialogue: MyDialogue, full_name: String, msg: Message) -> HandlerResult {
     match msg.text() {
         Some(text) => {
             let mut hasher = DefaultHasher::new();
             text.hash(&mut hasher);
-
+        loop {
+            //todo secure usernames.
             if valid_usernames().contains(&hasher.finish()) {
                 // invite new user
                 bot.send_message(msg.chat.id, format!("{} is a valid username", text)).await?;
+                let referee = <Recipient as From<String>>::from(text.to_string());
+                bot.create_chat_invite_link(referee.clone()).await?;
+                dialogue.update(State::Location {full_name, referee}).await?;
+                bot.send_message(msg.chat.id, "I have invited this user to the group, please make sure they join as, for not, your responses will not be saved otherwise.").await?;
+                bot.send_message(msg.chat.id, "where are you based").await?;
 
+                break
             } else {
                 bot.send_message(msg.chat.id, format!("{} is not a valid username, please try again", text)).await?;
-                dialogue.update(State::WhoWith { full_name }).await?;
             }
+        }
+
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Send me plain text.").await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn ask_location(bot: Bot, dialogue: MyDialogue, full_name: String, msg: Message) -> HandlerResult {
+    match msg.text() {
+        Some(text) => {
+            let mut hasher = DefaultHasher::new();
+            text.hash(&mut hasher);
         }
         None => {
             bot.send_message(msg.chat.id, "Send me plain text.").await?;
@@ -127,6 +149,6 @@ async fn ask_who_with(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerRe
 
 fn valid_usernames() -> Vec<u64> {
     let mut hasher = DefaultHasher::new();
-    "alex".hash(&mut hasher);
+    "@alexanderfive".hash(&mut hasher);
     vec![hasher.finish()]
 }
